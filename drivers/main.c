49388bac4685f6a42e6ecd38b6ee2906609cd086
Riyas-Mastermind <mmohammedriyaz540@gmail.com>
2026-04-28 12:21:58 +0530

    Add main.c for STM32F401xx GPIO LED toggle example

---
+++ drivers/main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 28, 2026
+ *      Author: riyas
+ */
+
+#include <stm32f401xx.h>
+
+
+void delay(void)
+{
+	for (uint32_t i = 0; i < 500000; i++);
+}
+
+
+int main(void)
+{
+	GPIO_Handle_t GPIO_LED;
+
+	GPIO_LED.pGPIOx = GPIOA;
+	GPIO_LED.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+	GPIO_LED.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GPIO_LED.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIO_LED.GPIO_PinConfig.GPIO_PinOPtype = GPIO_OP_TYPE_PP;
+	GPIO_LED.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PerClkControl(GPIOA, ENABLE);
+
+	GPIO_Init(&GPIO_LED);
+
+	while(1)
+	{
+		GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+		delay();
+	}
+	return 0;
+}
+
+
